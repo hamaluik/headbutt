25458f1cb19ad94e07912b7c8074b0b6fa2310cc
Kenton Hamaluik <kenton@hamaluik.ca>
2020-02-15 12:21:42 -0700

    converted twod.Headbutt to be static with pure public functions

--- src/headbutt/twod/Headbutt.hx
+++ src/headbutt/twod/Headbutt.hx
@@ -17,7 +17,7 @@
 package headbutt.twod;
 
 import glm.Vec3;
-using glm.Vec2;
+import glm.Vec2;
 
 private class Edge {
     public var distance: Float;
@@ -42,40 +42,52 @@ private enum PolygonWinding {
     CounterClockwise;
 }
 
-class Headbutt {
-    private var vertices: Array<Vec2>;
-    private var direction: Vec2;
-    private var shapeA: Shape;
-    private var shapeB: Shape;
+typedef TestResult = {
+    simplex: Array<Vec2>,
+    colliding: Bool,
+    shapeA: Shape,
+    shapeB: Shape,
+};
 
+/**
+ Implementations of the GJK and EPA algorithms
+**/
+class Headbutt {
     /**
        The maximum number of simplex evolution iterations before we accept the
        given simplex. For non-curvy shapes, this can be low. Curvy shapes potentially
        require higher numbers, but this can introduce significant slow-downs at
        the gain of not much accuracy.
     */
-    public var maxIterations: Int = 20;
+    public static var MAX_ITERATIONS: Int = 20;
 
     /**
-       Create a new Headbutt instance. Headbutt needs to be instantiated because
-       internally it stores state. This may change in the future.
-    */
-    public function new() {}
+     The maximum number of iterations to employ when running the EPA algoritm, usually
+     required to be large-ish for circles
+    **/
+    public static var MAX_INTERSECTION_ITERATIONS: Int = 32;
 
-    private function calculateSupport(direction: Vec2): Vec2 {
-        var oppositeDirection: Vec2 = direction.multiplyScalar(-1, new Vec2());
-        var newVertex: Vec2 = shapeA.support(direction).copy(new Vec2());
-        newVertex.subtractVec(shapeB.support(oppositeDirection), newVertex);
+    /**
+     How accurate do intersection calculations need to be when using expanding polytopes
+    **/
+    public static var INTERSECTION_EPSILON: Float = 0.00000001;
+
+    function new() {}
+
+    static function calculateSupport(shapeA: Shape, shapeB: Shape, direction: Vec2): Vec2 {
+        var oppositeDirection: Vec2 = Vec2.multiplyScalar(direction, -1, new Vec2());
+        var newVertex: Vec2 = Vec2.copy(shapeA.support(direction), new Vec2());
+        Vec2.subtractVec(newVertex, shapeB.support(oppositeDirection), newVertex);
         return newVertex;
     }
 
-    private function addSupport(direction: Vec2): Bool {
-        var newVertex: Vec2 = calculateSupport(direction);
+    static function addSupport(vertices: Array<Vec2>, shapeA: Shape, shapeB: Shape, direction: Vec2): Bool {
+        var newVertex: Vec2 = calculateSupport(shapeA, shapeB, direction);
         vertices.push(newVertex);
         return Vec2.dot(direction, newVertex) >= 0;
     }
 
-    function tripleProduct(a: Vec2, b: Vec2, c: Vec2): Vec2 {
+    static function tripleProduct(a: Vec2, b: Vec2, c: Vec2): Vec2 {
         var A: Vec3 = new Vec3(a.x, a.y, 0);
         var B: Vec3 = new Vec3(b.x, b.y, 0);
         var C: Vec3 = new Vec3(c.x, c.y, 0);
@@ -86,16 +98,16 @@ class Headbutt {
         return new Vec2(second.x, second.y);
     }
 
-    private function evolveSimplex(): EvolveResult {
+    static function evolveSimplex(vertices: Array<Vec2>, shapeA: Shape, shapeB: Shape, direction: Vec2): EvolveResult {
         switch(vertices.length) {
-            case 0: {
-                direction = shapeB.centre - shapeA.centre;
+            case 0:  {
+                Vec2.subtractVec(shapeB.centre, shapeA.centre, direction);
             }
-            case 1: {
+            case 1:  {
                 // flip the direction
                 direction *= -1;
             }
-            case 2: {
+            case 2:  {
                 // line ab is the line formed by the first two vertices
                 var ab: Vec2 = vertices[1] - vertices[0];
                 // line a0 is the line from the first vertex to the origin
@@ -105,7 +117,7 @@ class Headbutt {
                 // to line ab in the direction of the origin
                 direction = tripleProduct(ab, a0, ab);
             }
-            case 3: {
+            case 3:  {
                 // calculate if the simplex contains the origin
                 var c0: Vec2 = vertices[2] * -1;
                 var bc: Vec2 = vertices[1] - vertices[2];
@@ -114,13 +126,13 @@ class Headbutt {
                 var bcNorm: Vec2 = tripleProduct(ca, bc, bc);
                 var caNorm: Vec2 = tripleProduct(bc, ca, ca);
 
-                if(bcNorm.dot(c0) > 0) {
+                if(Vec2.dot(bcNorm, c0) > 0) {
                     // the origin is outside line bc
                     // get rid of a and add a new support in the direction of bcNorm
                     vertices.remove(vertices[0]);
                     direction = bcNorm;
                 }
-                else if(caNorm.dot(c0) > 0) {
+                else if(Vec2.dot(caNorm, c0) > 0) {
                     // the origin is outside line ca
                     // get rid of b and add a new support in the direction of caNorm
                     vertices.remove(vertices[1]);
@@ -132,12 +144,12 @@ class Headbutt {
                     return EvolveResult.FoundIntersection;
                 }
             }
-            case _: throw 'Can\'t have simplex with ${vertices.length} verts!';
+            case _:  throw 'Can\'t have simplex with ${vertices.length} verts!';
         }
 
-        return addSupport(direction)
+        return addSupport(vertices, shapeA, shapeB, direction)
             ? EvolveResult.StillEvolving
-            : EvolveResult.NoIntersection;
+            :  EvolveResult.NoIntersection;
     }
 
     /**
@@ -146,23 +158,26 @@ class Headbutt {
        @param shapeB 
        @return Bool
     */
-    public function test(shapeA: Shape, shapeB: Shape): Bool {
-        // reset everything
-        this.vertices = new Array<Vec2>();
-        this.shapeA = shapeA;
-        this.shapeB = shapeB;
+    public static function test(shapeA: Shape, shapeB: Shape): TestResult {
+        var vertices: Array<Vec2> = [];
+        var direction: Vec2 = new Vec2();
 
         // do the actual test
         var result: EvolveResult = EvolveResult.StillEvolving;
         var iterations: Int = 0;
-        while(iterations < maxIterations && result == EvolveResult.StillEvolving) {
-            result = evolveSimplex();
+        while(iterations < MAX_ITERATIONS && result == EvolveResult.StillEvolving) {
+            result = evolveSimplex(vertices, shapeA, shapeB, direction);
             iterations++;
         }
-        return result == EvolveResult.FoundIntersection;
+        return {
+            simplex: vertices,
+            colliding: result == EvolveResult.FoundIntersection,
+            shapeA: shapeA,
+            shapeB: shapeB,
+        };
     }
 
-    private function findClosestEdge(winding: PolygonWinding): Edge {
+    static function findClosestEdge(vertices: Array<Vec2>, winding: PolygonWinding): Edge {
         var closestDistance: Float = Math.POSITIVE_INFINITY;
         var closestNormal: Vec2 = new Vec2();
         var closestIndex: Int = 0;
@@ -171,8 +186,8 @@ class Headbutt {
             var j: Int = i + 1;
             if(j >= vertices.length) j = 0;
 
-            vertices[j].copy(line);
-            line.subtractVec(vertices[i], line);
+            Vec2.copy(vertices[j], line);
+            Vec2.subtractVec(line, vertices[i], line);
 
             var norm: Vec2 = switch(winding) {
                 case PolygonWinding.Clockwise: 
@@ -180,10 +195,10 @@ class Headbutt {
                 case PolygonWinding.CounterClockwise: 
                     new Vec2(-line.y, line.x);
             }
-            norm.normalize(norm);
+            Vec2.normalize(norm, norm);
 
             // calculate how far away the edge is from the origin
-            var dist: Float = norm.dot(vertices[i]);
+            var dist: Float = Vec2.dot(norm, vertices[i]);
             if(dist < closestDistance) {
                 closestDistance = dist;
                 closestNormal = norm;
@@ -197,39 +212,37 @@ class Headbutt {
     /**
        Given two shapes, test whether they overlap or not. If they don't, returns
        `null`. If they do, calculates the penetration vector and returns it.
-       @param shapeA 
-       @param shapeB 
+       @param testResult The return value from `test`
        @return Null<Vec2>
     */
-    public function intersect(shapeA: Shape, shapeB: Shape): Null<Vec2> {
-        // first, calculate the base simplex
-        if(!test(shapeA, shapeB)) {
+    public static function intersect(testResult: TestResult): Null<Vec2> {
+        if(!testResult.colliding) {
             // if we're not intersecting, return null
             return null;
         }
 
         // calculate the winding of the existing simplex
-        var e0: Float = (vertices[1].x - vertices[0].x) * (vertices[1].y + vertices[0].y);
-        var e1: Float = (vertices[2].x - vertices[1].x) * (vertices[2].y + vertices[1].y);
-        var e2: Float = (vertices[0].x - vertices[2].x) * (vertices[0].y + vertices[2].y);
+        var e0: Float = (testResult.simplex[1].x - testResult.simplex[0].x) * (testResult.simplex[1].y + testResult.simplex[0].y);
+        var e1: Float = (testResult.simplex[2].x - testResult.simplex[1].x) * (testResult.simplex[2].y + testResult.simplex[1].y);
+        var e2: Float = (testResult.simplex[0].x - testResult.simplex[2].x) * (testResult.simplex[0].y + testResult.simplex[2].y);
         var winding: PolygonWinding =
             if(e0 + e1 + e2 >= 0) PolygonWinding.Clockwise;
             else PolygonWinding.CounterClockwise;
 
         var intersection: Vec2 = new Vec2();
-        for(i in 0...32) {
-            var edge: Edge = findClosestEdge(winding);
-            var support: Vec2 = calculateSupport(edge.normal);
-            var distance: Float = support.dot(edge.normal);
+        for(_ in 0...MAX_INTERSECTION_ITERATIONS) {
+            var edge: Edge = findClosestEdge(testResult.simplex, winding);
+            var support: Vec2 = calculateSupport(testResult.shapeA, testResult.shapeB, edge.normal);
+            var distance: Float = Vec2.dot(support, edge.normal);
 
-            intersection = edge.normal.copy(intersection);
-            intersection.multiplyScalar(distance, intersection);
+            Vec2.copy(edge.normal, intersection);
+            Vec2.multiplyScalar(intersection, distance, intersection);
 
-            if(Math.abs(distance - edge.distance) <= 0.000001) {
+            if(Math.abs(distance - edge.distance) <= INTERSECTION_EPSILON) {
                 return intersection;
             }
             else {
-                vertices.insert(edge.index, support);
+                testResult.simplex.insert(edge.index, support);
             }
         }
 
--- test/TestHeadbutt2D.hx
+++ test/TestHeadbutt2D.hx
@@ -13,98 +13,90 @@ import glm.Vec2;
 class TestHeadbutt2D extends BuddySuite {
     public function new() {
         describe('Using Headbutt 2D', {
-            var hb:Headbutt;
-            beforeEach({
-                hb = new Headbutt();
-            });
-
             it('should calculate Minkowski difference supports with polygons', {
-                var shapeA:Polygon = new Polygon([
+                var shapeA: Polygon = new Polygon([
                     new Vec2(-1,  1), new Vec2( 1,  1),
                     new Vec2(-1, -1), new Vec2( 1, -1)
                 ]);
-                var shapeB:Polygon = new Polygon([
+                var shapeB: Polygon = new Polygon([
                     new Vec2(-1,  1), new Vec2( 1,  1),
                     new Vec2(-1, -1), new Vec2( 1, -1)
                 ]);
 
-                hb.shapeA = shapeA;
-                hb.shapeB = shapeB;
-
-                var verts:Array<Vec2> = new Array<Vec2>();
+                var verts: Array<Vec2> = new Array<Vec2>();
                 for(a in shapeA.vertices) {
                     for(b in shapeB.vertices) {
                         verts.push(new Vec2(a.x - b.x, a.y - b.y));
                     }
                 }
-                var md:Polygon = new Polygon(verts);
+                var md: Polygon = new Polygon(verts);
                 
-                var dirs:Array<Vec2> = [
+                var dirs: Array<Vec2> = [
                     new Vec2(-1, 1), new Vec2(0, 1), new Vec2(1, 1),
                     new Vec2(-1, 0), new Vec2(1, 0),
                     new Vec2(-1, -1), new Vec2(0, -1), new Vec2(1, -1)
                 ];
                 for(dir in dirs) {
-                    var mdSupport:Vec2 = md.support(dir);
-                    var support:Vec2 = hb.calculateSupport(dir);
+                    var mdSupport: Vec2 = md.support(dir);
+                    var support: Vec2 = Headbutt.calculateSupport(shapeA, shapeB, dir);
                     support.x.should.be(mdSupport.x);
                     support.y.should.be(mdSupport.y);
                 }
             });
 
             it('should detect collisions for two polygons which overlap', {
-                var shapeA:Polygon = new Polygon([
+                var shapeA: Polygon = new Polygon([
                     new Vec2(-1,  1), new Vec2( 1,  1),
                     new Vec2(-1, -1), new Vec2( 1, -1)
                 ]);
-                var shapeB:Polygon = new Polygon([
+                var shapeB: Polygon = new Polygon([
                     new Vec2(-1,  1), new Vec2( 1,  1),
                     new Vec2(-1, -1), new Vec2( 1, -1)
                 ]);
                 shapeB.setTransform(new Vec2(0.5, 0.5), 0, new Vec2(1, 1));
 
-                var result:Bool = hb.test(shapeA, shapeB);
+                var result: Bool = Headbutt.test(shapeA, shapeB).colliding;
                 result.should.be(true);
             });
 
             it('shouldn\'t detect collisions for two polygons which don\'t overlap', {
-                var shapeA:Polygon = new Polygon([
+                var shapeA: Polygon = new Polygon([
                     new Vec2(-1,  1), new Vec2( 1,  1),
                     new Vec2(-1, -1), new Vec2( 1, -1)
                 ]);
-                var shapeB:Polygon = new Polygon([
+                var shapeB: Polygon = new Polygon([
                     new Vec2(-1,  1), new Vec2( 1,  1),
                     new Vec2(-1, -1), new Vec2( 1, -1)
                 ]);
                 shapeB.setTransform(new Vec2(5, 0), 0, new Vec2(1, 1));
 
-                var result:Bool = hb.test(shapeA, shapeB);
+                var result: Bool = Headbutt.test(shapeA, shapeB).colliding;
                 result.should.be(false);
             });
 
             it('shouldn\'t matter what order the polygons go in', {
-                var shapeA:Polygon = new Polygon([
+                var shapeA: Polygon = new Polygon([
                     new Vec2(-1,  1), new Vec2( 1,  1),
                     new Vec2(-1, -1), new Vec2( 1, -1)
                 ]);
-                var shapeB:Polygon = new Polygon([
+                var shapeB: Polygon = new Polygon([
                     new Vec2(-1,  1), new Vec2( 1,  1),
                     new Vec2(-1, -1), new Vec2( 1, -1)
                 ]);
                 shapeB.setTransform(new Vec2(0.5, 0.5), 0, new Vec2(1, 1));
 
-                var resultA:Bool = hb.test(shapeA, shapeB);
-                var resultB:Bool = hb.test(shapeB, shapeA);
+                var resultA: Bool = Headbutt.test(shapeA, shapeB).colliding;
+                var resultB: Bool = Headbutt.test(shapeB, shapeA).colliding;
                 resultA.should.be(true);
                 resultB.should.be(true);
             });
 
             it('should detect collisions between a shape and itself', {
-                var shapeA:Polygon = new Polygon([
+                var shapeA: Polygon = new Polygon([
                     new Vec2(-1,  1), new Vec2( 1,  1),
                     new Vec2(-1, -1), new Vec2( 1, -1)
                 ]);
-                var result:Bool = hb.test(shapeA, shapeA);
+                var result: Bool = Headbutt.test(shapeA, shapeA).colliding;
                 result.should.be(true);
             });
 
@@ -112,41 +104,41 @@ class TestHeadbutt2D extends BuddySuite {
                 var lineA = new Line(new Vec2(-1, -1), new Vec2(1, 1));
                 var lineB = new Line(new Vec2(-1, 1), new Vec2(1, -1));
 
-                var result:Bool = hb.test(lineA, lineB);
+                var result: Bool = Headbutt.test(lineA, lineA).colliding;
                 result.should.be(true);
             });
 
             it('should detect collisions between a line and a polygon', {
-                var shapeA:Polygon = new Polygon([
+                var shapeA: Polygon = new Polygon([
                     new Vec2(-1, -1), new Vec2(1, -1),
                     new Vec2(1, 1), new Vec2(-1, 1)
                 ]);
                 var lineA = new Line(new Vec2(-1, -1), new Vec2(1, 1));
 
-                var result:Bool = hb.test(shapeA, lineA);
+                var result: Bool = Headbutt.test(shapeA, lineA).colliding;
                 result.should.be(true);
             });
 
             it('should detect collisions between a line and a circle', {
-                var circ:Circle = new Circle(new Vec2(0, 0), 1);
+                var circ: Circle = new Circle(new Vec2(0, 0), 1);
                 var lineA = new Line(new Vec2(-1, -1), new Vec2(1, 1));
                 var lineB = new Line(new Vec2(-5, -5), new Vec2(-4, -4));
 
-                var resultA:Bool = hb.test(circ, lineA);
-                var resultB:Bool = hb.test(circ, lineB);
+                var resultA: Bool = Headbutt.test(circ, lineA).colliding;
+                var resultB: Bool = Headbutt.test(circ, lineB).colliding;
 
                 resultA.should.be(true);
                 resultB.should.be(false);
             });
 
             it('should detect collisions between a polygon and a circle', {
-                var circ:Circle = new Circle(new Vec2(0.25, 0), 1);
-                var square:Polygon = new Polygon([
+                var circ: Circle = new Circle(new Vec2(0.25, 0), 1);
+                var square: Polygon = new Polygon([
                     new Vec2(-1, -1), new Vec2(1, -1),
                     new Vec2(1, 1), new Vec2(-1, 1)
                 ]);
 
-                var result:Bool = hb.test(circ, square);
+                var result: Bool = Headbutt.test(circ, square).colliding;
                 result.should.be(true);
             });
 
@@ -155,21 +147,21 @@ class TestHeadbutt2D extends BuddySuite {
                 var squareB: Rectangle = new Rectangle(new Vec2(2, 2));
                 squareB.setTransform(new Vec2(1.5, 0), 0, new Vec2(1, 1));
 
-                var intersection:Null<Vec2> = hb.intersect(squareA, squareB);
+                var intersection: Null<Vec2> = Headbutt.intersect(Headbutt.test(squareA, squareB));
                 intersection.should.not.be(null);
                 intersection.x.should.beCloseTo(0.5);
                 intersection.y.should.beCloseTo(0);
             });
 
             it('should calculate the intersection of two circles', {
-                var circA:Circle = new Circle(new Vec2(0, 0), 1);
-                var circB:Circle = new Circle(new Vec2(1, 1), 0.5);
+                var circA: Circle = new Circle(new Vec2(0, 0), 1);
+                var circB: Circle = new Circle(new Vec2(1, 1), 0.5);
                 
-                var intersection:Vec2 = hb.intersect(circA, circB);
+                var intersection: Null<Vec2> = Headbutt.intersect(Headbutt.test(circA, circB));
 
                 // calculate the intersection manually
-                var ix:Float = circA.radius * Math.cos(Math.PI / 4) - (circB.radius * Math.cos(5 * Math.PI / 4) + circB.centre.x);
-                var iy:Float = circA.radius * Math.sin(Math.PI / 4) - (circB.radius * Math.sin(5 * Math.PI / 4) + circB.centre.y);
+                var ix: Float = circA.radius * Math.cos(Math.PI / 4) - (circB.radius * Math.cos(5 * Math.PI / 4) + circB.centre.x);
+                var iy: Float = circA.radius * Math.sin(Math.PI / 4) - (circB.radius * Math.sin(5 * Math.PI / 4) + circB.centre.y);
                 intersection.x.should.beCloseTo(ix);
                 intersection.y.should.beCloseTo(iy);
             });
@@ -178,7 +170,7 @@ class TestHeadbutt2D extends BuddySuite {
                 var square: Rectangle = new Rectangle(new Vec2(2, 2));
                 var line: Line = new Line(new Vec2(0.5, 0), new Vec2(3.5, 3));
 
-                var intersection:Vec2 = hb.intersect(square, line);
+                var intersection: Null<Vec2> = Headbutt.intersect(Headbutt.test(square, line));
                 intersection.x.should.be(0.5);
                 intersection.y.should.be(0);
             });
@@ -188,15 +180,15 @@ class TestHeadbutt2D extends BuddySuite {
                 var squareB: Rectangle = new Rectangle(new Vec2(2, 2));
 
                 squareB.setTransform(new Vec2(2.1, 0), 0, new Vec2(1, 1));
-                var hit: Bool = hb.test(squareA, squareB);
+                var hit: Bool = Headbutt.test(squareA, squareB).colliding;
                 hit.should.be(false);
 
                 squareB.setTransform(new Vec2(2.1, 0), Math.PI / 4, new Vec2(1, 1));
-                var hit: Bool = hb.test(squareA, squareB);
+                var hit: Bool = Headbutt.test(squareA, squareB).colliding;
                 hit.should.be(true);
 
                 squareB.setTransform(new Vec2(2.1, 0), 0, new Vec2(1.5, 1));
-                var hit: Bool = hb.test(squareA, squareB);
+                var hit: Bool = Headbutt.test(squareA, squareB).colliding;
                 hit.should.be(true);
             });
         });
